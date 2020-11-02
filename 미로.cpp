9aa503dd4b08501291fdb1b03381b27c497ff446
unknown <sand8594@hufs.ac.kr>
2020-11-02 22:20:38 +0900

    미로이동

--- 미로.cpp
+++ 미로.cpp
@@ -8,16 +8,31 @@ using namespace std;
 3 도착점
 */
 
-
-void SetMaze(char Maze[21][21])
+struct _tagPoint
 {
+	int x;
+	int y;
+};
+typedef _tagPoint POINT;
+typedef _tagPoint* PPOINT;
+
+void SetMaze(char Maze[21][21], PPOINT pPlayerPos, PPOINT pStartPos, PPOINT pEndPos)
+{
+	pStartPos->x = 0;
+	pStartPos->y = 0;
+
+	pEndPos->x = 19;
+	pEndPos->y = 19;
+
+	*pPlayerPos = *pStartPos;
+
 	strcpy_s(Maze[0], "21100000000000000000");
-	strcpy_s(Maze[1], "00111111111000000000");
-	strcpy_s(Maze[2], "00100010001111111100");
+	strcpy_s(Maze[1], "00111111111100000000");
+	strcpy_s(Maze[2], "00100010000111111100");
 	strcpy_s(Maze[3], "01100010000000000100");
 	strcpy_s(Maze[4], "01000011110001111100");
 	strcpy_s(Maze[5], "01000000001111000000");
-	strcpy_s(Maze[6], "01100000001000000000");
+	strcpy_s(Maze[6], "01100000001111000000");
 	strcpy_s(Maze[7], "00100000001111111000");
 	strcpy_s(Maze[8], "00001110000000001000");
 	strcpy_s(Maze[9], "01111011111111111000");
@@ -32,17 +47,20 @@ void SetMaze(char Maze[21][21])
 	strcpy_s(Maze[18], "01000000000001100000");
 	strcpy_s(Maze[19], "11000000000000111113");
 }
-void Output(char Maze[21][21])
+void Output(char Maze[21][21], PPOINT pPlayerPos)
 {
 	for (int i = 0; i < 20; i++)
 	{
 		for (int j = 0; j < 20; j++)
 		{
-			if (Maze[i][j] == '0')
+			if (pPlayerPos->x == j && pPlayerPos->y == i)
+				cout << "☆";
+
+			else if (Maze[i][j] == '0')
 				cout << "■";
 
 			else if (Maze[i][j] == '1')
-				cout << " ";
+				cout << "  ";
 
 			else if (Maze[i][j] == '2')
 				cout << "★";
@@ -54,22 +72,100 @@ void Output(char Maze[21][21])
 		cout << endl;
 	}
 }
+
+void MoveUp(char Maze[21][21], PPOINT pPlayerPos)
+{
+	if (pPlayerPos->y - 1 >= 0)
+	{
+		if (Maze[pPlayerPos->y - 1][pPlayerPos->x] != '0')
+		{
+			--pPlayerPos->y;
+		}
+	}
+	
+}
+void MoveDown(char Maze[21][21], PPOINT pPlayerPos)
+{
+	if (pPlayerPos->y + 1 < 20)
+	{
+		if (Maze[pPlayerPos->y + 1][pPlayerPos->x] != '0')
+		{
+			++pPlayerPos->y;
+		}
+	}
+}
+void MoveRight(char Maze[21][21], PPOINT pPlayerPos)
+{
+	if (pPlayerPos->x + 1 < 20)
+	{
+		if (Maze[pPlayerPos->y][pPlayerPos->x +1] != '0')
+		{
+			++pPlayerPos->x;
+		}
+	}
+}
+void MoveLeft(char Maze[21][21], PPOINT pPlayerPos)
+{
+	if (pPlayerPos->x - 1 < 20)
+	{
+		if (Maze[pPlayerPos->y][pPlayerPos->x - 1] != '0')
+		{
+			--pPlayerPos->x;
+		}
+	}
+}
+void MovePlayer(char Maze[21][21], PPOINT pPlayerPos, char cInput)
+{
+	switch (cInput)
+	{
+	case 'w':
+	case 'W':
+		MoveUp(Maze, pPlayerPos);
+		break;
+	case 's':
+	case 'S':
+		MoveDown(Maze, pPlayerPos);
+		break;
+	case 'a':
+	case 'A':
+		MoveLeft(Maze, pPlayerPos);
+		break;
+	case 'd':
+	case 'D':
+		MoveRight(Maze, pPlayerPos);
+		break;
+
+	}
+}
+
 int main() 
 {
 	char strMaze[21][21] = {};
-	SetMaze(strMaze);
+
+	POINT tPlayerPos;
+	POINT tStartPos;
+	POINT tEndPos;
+
+	SetMaze(strMaze, &tPlayerPos, &tStartPos, &tEndPos);
 
 	while (true)
 	{
 		system("cls");
-		Output(strMaze);
+		Output(strMaze, &tPlayerPos);
+
+		if (tPlayerPos.x == tEndPos.x && tPlayerPos.y == tEndPos.y)
+		{
+			cout << "도착했습니다" << endl;
+			break;
+		}
 		cout << "w : 위 s : 아래 a : 왼쪽 d : 오른쪽 q : 종료 : ";
 		char cInput = _getch();
 
 		if (cInput == 'q' || cInput == 'Q')
 			break;
+
+		MovePlayer(strMaze, &tPlayerPos, cInput);
 	}
-	Output(strMaze);
 
 	return 0;
 }
